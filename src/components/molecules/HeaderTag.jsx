c09eeb25b76f60fa960b9383986c8c09752d0fa2
tatsuya <clarkmaster@icloud.com>
2024-06-02 14:24:07 +0900

    eslint prettier

--- src/components/molecules/HeaderTag.jsx
+++ src/components/molecules/HeaderTag.jsx
@@ -1,63 +1,61 @@
-import { useDispatch, useSelector } from "react-redux"
-import styled from "styled-components"
-import { activeTag } from "../../redux/headerTagSlice";
+import styled from 'styled-components'
+import { useState } from 'react'
 
 export const HeaderTag = () => {
-  const active = useSelector((state) => state.headerTag.isStateActive);
-  const dispatch = useDispatch();
+  const [active, setActive] = useState(true)
 
-  const onClickIssue = () => {
-    dispatch(activeTag(true));
-  };
+  const onClickTag = () => {
+    setActive((prevState) => !prevState)
+  }
 
-  const onClickPullRequest = () => {
-    dispatch(activeTag(false));
-  };
-  
-  return(
+  return (
     <div>
-    <SHeaderTagWrapper>
-      <SHeaderTagLeft active={active.toString()} onClick={onClickIssue} ><SHeaderTagSpan>Issue</SHeaderTagSpan></SHeaderTagLeft>
-      <SHeaderTagRight active={active.toString()} onClick={onClickPullRequest} ><SHeaderTagSpan>Pull Request</SHeaderTagSpan></SHeaderTagRight>
-    </SHeaderTagWrapper>
+      <SHeaderTagWrapper>
+        <SHeaderTagLeft active={active ? 'true' : ''} onClick={active ? undefined : onClickTag}>
+          <SHeaderTagSpan>Issue</SHeaderTagSpan>
+        </SHeaderTagLeft>
+        <SHeaderTagRight active={!active ? 'true' : ''} onClick={active ? onClickTag : undefined}>
+          <SHeaderTagSpan>Pull Request</SHeaderTagSpan>
+        </SHeaderTagRight>
+      </SHeaderTagWrapper>
     </div>
   )
 }
 
 const SHeaderTagWrapper = styled.ul`
-display: flex;
-padding: 0px;
-margin: 0px;
+  display: flex;
+  padding: 0px;
+  margin: 0px;
 `
 
 const SHeaderTagLeft = styled.li`
-font-size: 1.2rem;
-width: 100%;
-text-align: center;
-border-top: ${(props) => props.active === "true"? `1px solid rgb(225, 228, 232)`: `initial`};
-border-right: ${(props) => props.active === "true"? `1px solid rgb(225, 228, 232)`: `initial`};
-border-left: ${(props) => props.active === "true"? `1px solid rgb(225, 228, 232)`: `initial`};
-border-bottom: ${(props) => props.active === "true"? `initial`: `1px solid rgb(225, 228, 232)`};
-border-radius:6px 6px 0px 0px;
-list-style-type: none;
+  font-size: 1.2rem;
+  width: 100%;
+  text-align: center;
+  border-top: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-right: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-left: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-bottom: ${(props) => (`${props.active}` ? `initial` : `1px solid rgb(225, 228, 232)`)};
+  border-radius: 6px 6px 0px 0px;
+  list-style-type: none;
 `
 
 const SHeaderTagRight = styled.li`
-font-size: 1.2rem;
-width: 100%;
-text-align: center;
-border-top: ${(props) => props.active === "true"? `initial`: `1px solid rgb(225, 228, 232)`};
-border-right: ${(props) => props.active === "true"? `initial`: `1px solid rgb(225, 228, 232)`};
-border-left: ${(props) => props.active === "true"? `initial`: `1px solid rgb(225, 228, 232)`};
-border-bottom: ${(props) => props.active === "true"? `1px solid rgb(225, 228, 232)`: `initial`};
-border-radius:6px 6px 0px 0px;
-list-style-type: none;
+  font-size: 1.2rem;
+  width: 100%;
+  text-align: center;
+  border-top: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-right: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-left: ${(props) => (`${props.active}` ? `1px solid rgb(225, 228, 232)` : `initial`)};
+  border-bottom: ${(props) => (`${props.active}` ? `initial` : `1px solid rgb(225, 228, 232)`)};
+  border-radius: 6px 6px 0px 0px;
+  list-style-type: none;
 `
 
 const SHeaderTagSpan = styled.span`
-cursor: pointer;
-color: rgb(88, 96, 105);
-padding: 16px;
-display: block;
-width: 100%;
+  cursor: pointer;
+  color: rgb(88, 96, 105);
+  padding: 16px;
+  display: block;
+  width: 100%;
 `
--- src/components/molecules/HeaderTitles.jsx
+++ src/components/molecules/HeaderTitles.jsx
@@ -1,31 +1,33 @@
-import { Link } from "react-router-dom";
-import styled from "styled-components";
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
 export const HeaderTitles = () => {
-
-  return(
+  return (
     <SHeaderTitles>
-      <SHeaderLists ><Link to="/issue">Issue</Link></SHeaderLists>
-      <SHeaderLists><Link to="/pullRequest">Pull Request</Link></SHeaderLists>
+      <SHeaderLists>
+        <Link to="/issue">Issue</Link>
+      </SHeaderLists>
+      <SHeaderLists>
+        <Link to="/pullRequest">Pull Request</Link>
+      </SHeaderLists>
     </SHeaderTitles>
-  );
-};
+  )
+}
 
 const SHeaderTitles = styled.ul`
-display: flex;
-width: 100%;
-padding: 0px 32px
-margin: 0px;
-`;
+  display: flex;
+  width: 100%;
+  padding: 0px 32px
+  margin: 0px;
+`
 
 const SHeaderLists = styled.li`
-margin-right: 16px;
-list-style-type: none;
-color: white;
-font-size: 1.1rem;
-a{
-  color: inherit;
-  text-decoration: none;
-  
-}
+  margin-right: 16px;
+  list-style-type: none;
+  color: white;
+  font-size: 1.1rem;
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
 `
