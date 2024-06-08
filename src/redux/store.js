e33cbe0f7b53844bd638e0a126c4d537da077d36
tatsuya <clarkmaster@icloud.com>
2024-06-08 14:00:54 +0900

    Fixing packageJson

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit';
-import drawerReducer from './drawerSlice';
-import headerTagReducer from './headerTagSlice';
 import issueReducer from './issueSlice';
 import checkBoxReducer from './checkBoxSlice';
 
 export const store = configureStore({
   reducer: {
-    drawer: drawerReducer,
-    headerTag: headerTagReducer,
     issues: issueReducer,
     checkBox: checkBoxReducer,
   },
